cae9ed44fe03aa34e682aafcf9a07a3bf752bdc8
Florian Ober <8720518+florianober@users.noreply.github.com>
2024-05-23 12:43:42 +0200

    gtk_hl_dialog: bugfix
    if providing artists or documenters, the cptr length was calculated from the numbers of authors.

--- src/gtk-hl-dialog.f90
+++ src/gtk-hl-dialog.f90
@@ -350,7 +350,7 @@ contains
           cptr(i) = c_loc(credit(1))
           nullify(credit)
        end do
-       cptr(size(authors)+1) = c_null_ptr
+       cptr(size(documenters)+1) = c_null_ptr
        call gtk_about_dialog_set_documenters(about, cptr)
        deallocate(cptr)
     end if
@@ -363,7 +363,7 @@ contains
           cptr(i) = c_loc(credit(1))
           nullify(credit)
        end do
-       cptr(size(authors)+1) = c_null_ptr
+       cptr(size(artists)+1) = c_null_ptr
        call gtk_about_dialog_set_artists(about, cptr)
        deallocate(cptr)
     end if
